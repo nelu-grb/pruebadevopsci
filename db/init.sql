868adeaa48b40ff5f2602e90d2b2bac20d57eed6
nelu <nels.baeza@duocuc.cl>
2026-05-15 17:01:03 -0400

    asdasd

--- db/init.sql
+++ db/init.sql
@@ -13,3 +13,6 @@ INSERT INTO productos (nombre, descripcion, precio, stock) VALUES
 ('Alimento Cachorro Premium', 'Sabor pollo, razas pequeñas', 19990, 15),
 ('Alimento Adulto Light', 'Control de peso, razas medianas', 17990, 8),
 ('Snacks Dentales', 'Ayuda a la limpieza dental', 5990, 30);
+
+
+//aaaa
