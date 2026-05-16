e0095ee86dd3e86fa00be040f0268c99c4c10277
nelu <nels.baeza@duocuc.cl>
2026-05-15 21:16:57 -0400

    funciona xfa

--- backend-ventas/Springboot-API-REST/src/main/java/com/citt/exceptions/dto/ErrorMessage.java
+++ backend-ventas/Springboot-API-REST/src/main/java/com/citt/exceptions/dto/ErrorMessage.java
@@ -1,14 +1,24 @@
 package com.citt.exceptions.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ErrorMessage {
     private HttpStatus status;
     private String message;
+
+    // Constructor vacío manual
+    public ErrorMessage() {}
+
+    // Constructor con argumentos manual (El que Maven exige)
+    public ErrorMessage(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    // Getters y Setters manuales
+    public HttpStatus getStatus() { return status; }
+    public void setStatus(HttpStatus status) { this.status = status; }
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
 }
