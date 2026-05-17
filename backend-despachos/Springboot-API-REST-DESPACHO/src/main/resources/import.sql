769662156a1d36c50d94795ef07a2fcc542bae8d
nelu <nels.baeza@duocuc.cl>
2026-05-17 19:52:29 -0400

    Refactor import.sql to update venta entries and clarify despacho_generado status for better data integrity

--- backend-despachos/Springboot-API-REST-DESPACHO/src/main/resources/import.sql
+++ backend-despachos/Springboot-API-REST-DESPACHO/src/main/resources/import.sql
@@ -1,11 +1,10 @@
 -- Forzar a la base de datos a resetear el contador auto-incremental a 1 siempre
 ALTER TABLE despacho AUTO_INCREMENT = 1;
 
--- Mapeo exacto con las primeras ventas correspondientes (repartidas en entregadas y pendientes)
+-- Mapeo exacto amarrado a las compras de la 6 a la 10 (todas en ruta / pendientes de entrega)
 INSERT INTO despacho (fecha_despacho, patente_camion, intento, id_compra, direccion_compra, valor_compra, despachado) VALUES 
-('2026-05-16', 'HG-TL-84', 1, 1, 'Av. Vicuña Mackenna 4860, San Joaquín', 25990.00, 0),
-('2026-05-16', 'FT-XW-12', 2, 2, 'Pasaje El Romeral 124, La Florida', 12450.00, 0),
-('2026-05-16', 'XD-PT-99', 0, 3, 'Calle Las Brisas 890, Puente Alto', 45800.00, 0),
-('2026-05-17', 'FT-XW-12', 1, 4, 'Av. Américo Vespucio 1501, Cerrillos', 89990.00, 0),
-('2026-05-16', 'LB-CR-45', 1, 5, 'Gran Avenida 6540, La Cisterna', 35400.00, 1),
-('2026-05-15', 'GZ-LK-33', 2, 6, 'Av. Providencia 1420, Depto 402, Providencia', 7500.00, 1);
+('2026-05-17', 'GZ-LK-33', 0, 6, 'Av. Providencia 1420, Depto 402, Providencia', 7500.00, 0),
+('2026-05-17', 'JW-SP-88', 1, 7, 'Condominio Los Alerces Casa 12, Peñalolén', 19990.00, 0),
+('2026-05-18', 'HG-TL-84', 0, 8, 'Alameda 340, Santiago Centro', 15490.00, 0),
+('2026-05-18', 'LB-CR-45', 2, 9, 'Av. Las Condes 11200, Las Condes', 120500.00, 0),
+('2026-05-18', 'XD-PT-99', 1, 10, 'Calle Pajaritos 2450, Maipú', 28990.00, 0);
--- backend-ventas/Springboot-API-REST/src/main/resources/import.sql
+++ backend-ventas/Springboot-API-REST/src/main/resources/import.sql
@@ -1,24 +1,16 @@
 -- Forzar a la base de datos a resetear el contador auto-incremental a 1 siempre
 ALTER TABLE venta AUTO_INCREMENT = 1;
 
-INSERT INTO venta (direccion_compra, valor_compra, fecha_compra, despacho_generado) VALUES 
-('Av. Vicuña Mackenna 4860, San Joaquín', 25990.00, '2026-05-15', 1),
-('Pasaje El Romeral 124, La Florida', 12450.00, '2026-05-16', 1),
-('Calle Las Brisas 890, Puente Alto', 45800.00, '2026-05-16', 1),
-('Av. Américo Vespucio 1501, Cerrillos', 89990.00, '2026-05-16', 1),
-('Gran Avenida 6540, La Cisterna', 35400.00, '2026-05-16', 1),
-('Av. Providencia 1420, Depto 402, Providencia', 7500.00, '2026-05-16', 0),
-('Condominio Los Alerces Casa 12, Peñalolén', 19990.00, '2026-05-16', 0),
-('Alameda 340, Santiago Centro', 15490.00, '2026-05-17', 0),
-('Av. Las Condes 11200, Las Condes', 120500.00, '2026-05-17', 0),
-('Calle Pajaritos 2450, Maipú', 28990.00, '2026-05-17', 0),
-('San Diego 1025, Santiago Centro', 9990.00, '2026-05-17', 0),
-('Av. Vitacura 4500, Vitacura', 150000.00, '2026-05-17', 0),
-('Los Carrera 450, Quilpué', 42300.00, '2026-05-17', 0),
-('Av. Recoleta 230, Recoleta', 18500.00, '2026-05-17', 0),
-('Pasaje Las Torres 8920, Pudahuel', 22490.00, '2026-05-17', 0),
-('Av. Independencia 1425, Independencia', 31200.00, '2026-05-17', 0),
-('Calle El Líbano 4510, Macul', 13990.00, '2026-05-17', 0),
-('Av. Larraín 6700, La Reina', 54990.00, '2026-05-17', 0),
-('Camino Central 120, Lo Barnechea', 210000.00, '2026-05-17', 0),
-('Av. Marathon 1230, Ñuñoa', 47800.00, '2026-05-17', 0);
+-- Las primeras 5 listas para generar despacho (despacho_generado = false)
+INSERT INTO venta (id_venta, direccion_compra, fecha_compra, valor_compra, despacho_generado) VALUES (1, 'Av. Vicuña Mackenna 4860, San Joaquín', '2026-05-16', 25990, false);
+INSERT INTO venta (id_venta, direccion_compra, fecha_compra, valor_compra, despacho_generado) VALUES (2, 'Pasaje El Romeral 124, La Florida', '2026-05-16', 12450, false);
+INSERT INTO venta (id_venta, direccion_compra, fecha_compra, valor_compra, despacho_generado) VALUES (3, 'Calle Las Brisas 890, Puente Alto', '2026-05-16', 45800, false);
+INSERT INTO venta (id_venta, direccion_compra, fecha_compra, valor_compra, despacho_generado) VALUES (4, 'Av. Américo Vespucio 1501, Cerrillos', '2026-05-17', 89990, false);
+INSERT INTO venta (id_venta, direccion_compra, fecha_compra, valor_compra, despacho_generado) VALUES (5, 'Gran Avenida 6540, La Cisterna', '2026-05-16', 35400, false);
+
+-- De la 6 a la 10 ya procesadas (despacho_generado = true)
+INSERT INTO venta (id_venta, direccion_compra, fecha_compra, valor_compra, despacho_generado) VALUES (6, 'Av. Providencia 1420, Providencia', '2026-05-17', 7500, true);
+INSERT INTO venta (id_venta, direccion_compra, fecha_compra, valor_compra, despacho_generado) VALUES (7, 'Condominio Los Alerces Casa 12, Peñalolén', '2026-05-17', 19990, true);
+INSERT INTO venta (id_venta, direccion_compra, fecha_compra, valor_compra, despacho_generado) VALUES (8, 'Alameda 340, Santiago Centro', '2026-05-18', 15490, true);
+INSERT INTO venta (id_venta, direccion_compra, fecha_compra, valor_compra, despacho_generado) VALUES (9, 'Av. Las Condes 11200, Las Condes', '2026-05-18', 120500, true);
+INSERT INTO venta (id_venta, direccion_compra, fecha_compra, valor_compra, despacho_generado) VALUES (10, 'Calle Pajaritos 2450, Maipú', '2026-05-18', 28990, true);
